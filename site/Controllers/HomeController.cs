024c742c3cacfde307eaab8473bafd977d9ac5c1
georgiy-osvyatinskiy <georgiy.osvyatinskiy@gmail.com>
2017-07-29 17:19:08 +0300

    First attempts in C# + .NET progrmming.

---
+++ site/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using site.Models;
+
+namespace site.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookID = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Спасибо, " + purchase.Person + ", за покупку!"; 
+        }
+    }
+}
---
+++ site/Models/Purchase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace site.Models
+{
+    public class Purchase
+    {
+        public int PurchaseID { get; set; }
+        public string Person { get; set; }
+        public string Address { get; set; }
+        public int BookID { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
